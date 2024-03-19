be3bfb2682d0cc002d6c53da098d6eda55048da9
ANDREA VALENTINA ARANGO PECEROS <av.arangopeceros@gmail.com>
2024-03-18 22:58:18 -0500

    nose

---
+++ app.js
@@ -0,0 +1,13 @@
+let usuarioNombre = prompt("Cual es tu nombre?"); 
+let usuarioCorreo = prompt("Ingresa tu correo para recibir info y ofertas.")
+
+console.log("Bienvenid@ " + usuarioNombre);
+document.write("Bienvenid@ " + usuarioNombre);
+
+
+let color = prompt("Ingresa un color");
+if (color !== null) {
+ document.body.style.backgroundColor = color;
+}
+
+
